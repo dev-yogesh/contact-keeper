a53d5f15fcdc7fefd08ee851881948bdfa06e3d8
yogeshgupta279 <yogeshgupta279@gmail.com>
2020-11-28 15:46:15 +0530

    Contact keeper

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Contact Keeper</h1>
+      <p className='my-1'>A webapp to manage contacts</p>
+      <p className='p bg-dark'>
+        <strong>Version:</strong> 1.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
